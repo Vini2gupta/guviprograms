7c4ba0cac9bba680fa262b36fe93ceace9af7a11
Vini2gupta <simran.gupta_cs15@gla.ac.in>
2018-02-20 22:34:26 +0530

    Add files via upload

---
+++ even and odd.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("enter the no");
+	scanf("%d",&n);
+	if(n>0)
+	{
+
+	if(n%2==0)
+	{
+		printf("no is even");
+	}
+	else 
+	{
+			printf("no is odd");
+	}
+
+}
+}
